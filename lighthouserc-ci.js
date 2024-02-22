1a8fff09f9e18f2d0ca4474c8887a2045c270f12
Joan León <joan.leon@gmail.com>
2024-02-23 00:10:20 +0100

    add lighthouserc-ci.js config file

---
+++ lighthouserc-ci.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "npm run start",
+      numberOfRuns: 3,
+      url: [
+        "http://localhost:3000",
+        "http://localhost:3000/left-sidebar",
+        "http://localhost:3000/right-sidebar",
+        "http://localhost:3000/no-sidebar",
+      ],
+      // chromePath: '/usr/bin/google-chrome', // WSL
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.8 }],
+        "categories:accessibility": ["error", { minScore: 0.8 }],
+        "categories:best-practices": ["error", { minScore: 0.8 }],
+        "categories:seo": ["error", { minScore: 0.8 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
