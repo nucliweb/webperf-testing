128744914ec94f5950eaa4218086e5c0f5d5eb6b
Joan León <joan.leon@gmail.com>
2024-02-23 18:52:05 +0100

    WIP

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -14,9 +14,6 @@ module.exports = {
     assert: {
       assertions: {
         "categories:performance": ["error", { minScore: 0.8 }],
-        "categories:accessibility": ["error", { minScore: 0.8 }],
-        "categories:best-practices": ["error", { minScore: 0.8 }],
-        "categories:seo": ["error", { minScore: 0.8 }],
       },
     },
     upload: {
